70777c6e56050f96cf0a3bb443967c0af3ef90c2
Seif El Sheshtaway <youssufkhaled9@gmail.com>
2022-01-28 17:40:41 +0300

    Updates

---
+++ precache-manifest.c89894b615f5a45d4576324f6e6dba56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f657005f66878078e9f8d37124a7aed3",
+    "url": "/E-commerce_mobile_version_website/home/index.html"
+  },
+  {
+    "revision": "6f6a71222a2b713ebdce",
+    "url": "/E-commerce_mobile_version_website/home/static/js/2.926700c6.chunk.js"
+  },
+  {
+    "revision": "16493286e5c5b9ccdbb3",
+    "url": "/E-commerce_mobile_version_website/home/static/js/main.5807e84e.chunk.js"
+  },
+  {
+    "revision": "cac7bf56dd5241fb81bc",
+    "url": "/E-commerce_mobile_version_website/home/static/js/runtime-main.00691c9a.js"
+  }
+]);
